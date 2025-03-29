91f646055e8ef7f1331eeced0f996a949963d377
atlas <hivethehappy@gmail.com>
2025-03-29 23:16:41 +0000

    hamza

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:7600', // Your React app's origin
+  origin: 'https://atlasmarket-eight.vercel.app', // Your React app's origin
   credentials: true
 }));
 app.use(express.json());
