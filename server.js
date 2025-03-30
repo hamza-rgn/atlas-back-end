a170d7a9abbd14a7888dbfcc22fa5af3b22b261c
atlas <hivethehappy@gmail.com>
2025-03-30 01:59:38 +0000

    as

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
   
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
