f0fad767b909b4cc7e40720dc55cc81c2e08ca0c
atlas <hivethehappy@gmail.com>
2025-03-30 00:12:30 +0000

    as

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ mongoose.connect(process.env.MONGODB_URI, {
 const User = require('./models/User');
 const Order = require('./models/Order');
 const LandingPage = require('./models/LandingPage');
-
+app.get('/test',async(req,res)=>{
+  res.send('hello')
+})
 // In your login route (/api/auth/login)
 app.post('/api/auth/login', async (req, res) => {
     const { username, password } = req.body;
