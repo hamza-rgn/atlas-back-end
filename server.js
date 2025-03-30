04fbeeda66ba0d4d7ba4e480a0dc5fc579d05d5b
atlas <hivethehappy@gmail.com>
2025-03-30 02:23:05 +0000

    as

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors({
   origin: function (origin, callback) {
     // allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    
+    console.log(origin)
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
       return callback(new Error(msg), false);
